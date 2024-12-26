6153797b0aef517bf0ef8ca47ff4279f352e10f5
bambam0202 <omarabdelkader0000@gmail.com>
2024-12-26 12:26:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.addEventListener('keydown', (event) => {
   if (event.keyCode === 65) {
-    new Audio('sounds/65.mp3').play(); // A key
+    new Audio('65.mp3').play(); // A key
   } else if (event.keyCode === 87) {
-    new Audio('sounds/87.mp3').play(); // W key
+    new Audio('87.mp3').play(); // W key
   } else if (event.keyCode === 83) {
-    new Audio('sounds/83.mp3').play(); // S key
+    new Audio('83.mp3').play(); // S key
   } else if (event.keyCode === 70) {  // For key "F"
-    new Audio('sounds/70.mp3').play(); // F key
+    new Audio('70.mp3').play(); // F key
   } else if (event.keyCode === 68) {  // For key "D"
-    new Audio('sounds/68.mp3').play(); // D key plays sound 71.mp3
+    new Audio('68.mp3').play(); // D key plays sound 71.mp3
   } else if (event.keyCode === 71) {  // For key "G"
-    new Audio('sounds/51.mp3').play();
+    new Audio('51.mp3').play(); // G key
   }
 });
